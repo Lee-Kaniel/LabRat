864ca50bfe2cecaa786b94815a8b99c89eab1d53
Lee-Kaniel <lee.kaniel@gmail.com>
2025-05-04 22:56:03 +0300

    Update overview.py

--- LabRat/src/schemas/overview.py
+++ LabRat/src/schemas/overview.py
@@ -8,14 +8,15 @@ from LabRat.src.schemas.filter_flag import FilterFlag
 
 
 class Overview(BaseModel):
-    group: str = Field(default=None, alias='Group')
-    well: str = Field(..., alias='Well')
+    group: str = Field(default=None, alias='Group') # Experimental group name, e.g., Control or Treated
+    well: str = Field(..., alias='Well') # Well identifier (e.g., "A1", "B12")
     average_std: float = Field(default=None, alias='avg/std')
-    contraction_list: List[Contraction] = Field(default=[])
-    filter_flag: Optional[FilterFlag] = Field(default=None)
+    contraction_list: List[Contraction] = Field(default=[]) # List of all contractions recorded in this well
+    filter_flag: Optional[FilterFlag] = Field(default=None) # Optional flag indicating whether this overview should be deleted or updated
 
     @field_validator('well')
     def extract_well_name_from_folder_name(cls, value):
+        # Extracts the well name from folder/file
         pattern = r'Well\s+(\w+)'
         match = re.search(pattern, value)
         if match:
@@ -24,6 +25,7 @@ class Overview(BaseModel):
 
     @field_validator('group')
     def extract_group_name_from_folder_name(cls, value):
+        # Extracts the group name following the well
         pattern = r'\bWell\b \w+ (\w+)'
         match = re.search(pattern, value)
         if match:
