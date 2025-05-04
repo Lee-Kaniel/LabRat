db1306eba42c5f423b9cfc03eddbbd1fad7b624a
Lee-Kaniel <lee.kaniel@gmail.com>
2025-05-04 21:13:38 +0300

    Update amplitude_filter.py

--- LabRat/src/data_filters/amplitude_filter.py
+++ LabRat/src/data_filters/amplitude_filter.py
@@ -9,9 +9,9 @@ from LabRat.src.utils.safe_get import get_field_or_updated
 
 
 class AmplitudeFilter(BaseDataFilter):
-    STD_FROM_AMPLITUDE = 2
-    PARTIAL_AMPLITUDE = 0.25
-    STD_FROM_PEAK_TO_PEAK = 0.25
+    STD_FROM_AMPLITUDE = 2 # Min amplitude must be within 2 SDs of the mean
+    PARTIAL_AMPLITUDE = 0.25 # Min amplitude must be ≥ 25% of max amplitude
+    STD_FROM_PEAK_TO_PEAK = 0.25  # Threshold to consider deviation in timing as noise
 
     @staticmethod
     def find_closest_real_peak_to_peak(
@@ -29,9 +29,15 @@ class AmplitudeFilter(BaseDataFilter):
         return None
 
     def adjust_overview_by_noise(self, overview: Overview) -> Overview:
+        """
+        updates the peak to peak times after taking into account the peaks deleted for being noise errors
+        if a peak was deleted the time to peak of the following peak is updated to the corrected value
+        """
         absolute_peak_to_peak_list = []
         real_peak_to_peak_list = []
         backlog_peak_to_peak_time = 0
+        
+        # Construct cumulative timing lists for all contractions
         for i in range(len(overview.contraction_list)):
             peak_to_peak = get_field_or_updated(overview.contraction_list[i], "peak_to_peak_time", 0)
             absolute_peak_time = peak_to_peak + backlog_peak_to_peak_time
@@ -40,9 +46,11 @@ class AmplitudeFilter(BaseDataFilter):
             if overview.contraction_list[i].filter_flag != FilterFlag.DELETE:
                 real_peak_to_peak_list.append(absolute_peak_time)
 
+        # If there aren't enough real contractions to define, return unchanged
         if len(real_peak_to_peak_list) < 2 or not absolute_peak_to_peak_list:
             return overview
 
+        # Compute median and standard deviation of timing differences between real contractions
         differences_real_peak_to_peak_list = []
         for i in range(len(real_peak_to_peak_list) - 1):
             difference = real_peak_to_peak_list[i + 1] - real_peak_to_peak_list[i]
@@ -51,10 +59,11 @@ class AmplitudeFilter(BaseDataFilter):
         median_peak_to_peak = numpy.median(differences_real_peak_to_peak_list)
         std_peak_to_peak = numpy.std(differences_real_peak_to_peak_list)
 
+        # checks if the deleted peak is a noise error or a different type of error through peak to peak time misalignment
         for i in range(len(overview.contraction_list)):
             if overview.contraction_list[i].filter_flag == FilterFlag.DELETE:
                 update_flag = False
-                for condition in (True, False):
+                for condition in (True, False): # Try both forward and backward search
                     if closest_peak := self.find_closest_real_peak_to_peak(
                             index=i, absolute_peak_to_peak_list=absolute_peak_to_peak_list,
                             real_peak_to_peak_list=real_peak_to_peak_list, above=condition
@@ -65,6 +74,7 @@ class AmplitudeFilter(BaseDataFilter):
                             break
                 if update_flag:
                     try:
+                        # Updates the timing of the *next* contraction if deleted peak was found to be a noise error
                         peak_to_peak = get_field_or_updated(
                             overview.contraction_list[i], "peak_to_peak_time", 0
                         )
@@ -80,6 +90,11 @@ class AmplitudeFilter(BaseDataFilter):
         return overview
 
     def filter_overview_by_contraction_amplitude(self, overview: Overview) -> Overview:
+        """
+        Flags contractions for deletion if their amplitude is too low:
+        - Either < (mean - 2*SD)
+        - Or < 25% of the maximum contraction amplitude.
+        """
         amplitude_length_list = [contraction.contraction_amplitude for contraction in overview.contraction_list]
 
         if not amplitude_length_list:
@@ -99,6 +114,12 @@ class AmplitudeFilter(BaseDataFilter):
         return overview
 
     def filter_overview(self, overview: Overview) -> Overview:
+        """
+        Main entry point:
+        - Filters by amplitude first.
+        - Then checks peak to peak time misalignment. if found the error was noise error.
+        - If noise error time to peak of next peak is updated
+        """
         try:
             amplitude_filtered_overview = self.filter_overview_by_contraction_amplitude(overview=overview)
             return self.adjust_overview_by_noise(amplitude_filtered_overview)
